8297acb665f5a0844b3e64bd4c3ac09290859cd0
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-11-26 09:34:25 -0500

    cart 211 got x moving sliders

--- final/js/Creature.js
+++ final/js/Creature.js
@@ -1,6 +1,6 @@
 class Creature {
   constructor(){
-    this.x = 30;
+    this.x = 0;
     this.y = 0;
     this.noiseXIndex = random(1000);
     this.noiseYIndex = random(1000);
@@ -12,26 +12,41 @@ class Creature {
     this.xTarget;
     this.yTarget;
     this.minMaxRange = 10;
-    let mM = this.mineMaxRange;
-    this.xVecRange = createSlider(-mM, mM, lerp(-mM,mM,.5), mM/20);
-    this.xVecRange.style('position', 'fixed');
-    this.xVecRange.style('left', this.x+"px");
-    this.xVecRange.style('top', this.y+"px");
-    // this.xVecRange = document.getElementById("myRange");
-    // this.btn = document.createElement("BUTTON");
-    // this.xVecRange = document.createElement("INPUT");
-    // this.xVecRange.setAttribute("range", 20);
+    this.size = 200;
+
+    let mM = this.minMaxRange;
+    this.xSlider = createSlider(-mM, mM, lerp(-mM,mM,.5), mM/20);
+    this.xSlider.style('position', 'fixed');
+    this.xSlider.style('width', this.size+'px');
+    // this.xSlider.style('display',"none");
+
+    this.ySlider = createSlider(-mM, mM, lerp(-mM,mM,.5), mM/20);
+    this.ySlider.style('position', 'fixed');
+    this.ySlider.style('width', this.size+'px');
+    this.ySlider.style('height', this.size+'px');
+    this.ySlider.style('display',"none");
   }
-  update(){
-    // console.log('hey');
+  updateNoise(){
     this.noiseXIndex += this.noiseIncrement;
     this.noiseYIndex += this.noiseIncrement;
-    this.x = noise(this.noiseXIndex) * 1000;
-    console.log(this.x);
-    this.y = noise(this.noiseYIndex) * 1000;
+    this.x = noise(this.noiseXIndex) * windowWidth;
+    this.y = noise(this.noiseYIndex) * windowHeight;
+
+    const sliderXValue = map(this.x,0,windowWidth,-this.minMaxRange,this.minMaxRange);
+
+    this.xSlider.value(sliderXValue);
+    this.xSlider.style('left', this.x+"px");
+    this.xSlider.style('top', this.y+"px");
+
+    this.ySlider.style('left', this.x+"px");
+    this.ySlider.style('top', this.y+"px");
+  }
+  updateVectors(){
+    this.x =
+    this.y = noise(this.noiseYIndex) * windowHeight;
+
     this.xVecRange.style('left', this.x+"px");
     this.xVecRange.style('top', this.y+"px");
-
   }
   display(){
 
--- final/js/script.js
+++ final/js/script.js
@@ -1,13 +1,13 @@
 let creature = [];
 function setup(){
-  for (let i = 0; i < 20; i ++){
+  for (let i = 0; i < 10; i ++){
     creature[i] = new Creature();
   }
 }
 
 function draw(){
-  for (let i = 0; i < 20; i ++){
-    creature[i].update();
+  for (let i = 0; i < creature.length; i ++){
+    creature[i].updateNoise();
     // console.log("hey");
   }
 }
