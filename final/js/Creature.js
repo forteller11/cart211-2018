f7ae583eb2d419f296feebfcc6bc02f3742c29aa
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-11-26 11:35:34 -0500

    211 made acceleration decrease as creatures approach targets

--- final/js/Creature.js
+++ final/js/Creature.js
@@ -7,15 +7,18 @@ class Creature {
     this.noiseIncrement = random(.01);
     this.xVec = 1;
     this.yVec = 0;
-    this.angularAcceleration = 4;
-    this.acceleration = random(3);
+    this.angle = atan2(this.yVec,this.xVec);
+    this.angularAcceleration = .1;
+    this.maxAcceleration = random(.2,3);
+    this.minAcceleration = .01;
+    this.acceleration = this.maxAcceleration;
     this.xTarget;
     this.yTarget;
     this.minMaxRange = 10;
     this.size = random(20,100);
 
     let mM = this.minMaxRange;
-    this.xSlider = createSlider(-mM, mM, lerp(-mM,mM,.5), mM/50);
+    this.xSlider = createSlider(-PI, PI, 0, TWO_PI/60);
     this.xSlider.style('position', 'fixed');
     this.xSlider.style('width', this.size+'px');
     // this.xSlider.style('display',"none");
@@ -28,6 +31,7 @@ class Creature {
 
     this.radio = createRadio();
     this.radio.option('');
+
     this.radio.style('position', 'fixed');
     this.radio.style('left', this.x+"px");
     this.radio.style('top', this.y+"px");
@@ -52,6 +56,8 @@ class Creature {
 
     this.xTarget = (xT1+xT2)/2;
     this.yTarget = (yT1+yT2)/2;
+    // this.xTarget = mouseX;
+    // this.yTarget = mouseY;
   }
   updateVectors(){
     const steeringCorrection = 20; //1 = perfect correction every frame so vecs pointed towards target
@@ -65,11 +71,28 @@ class Creature {
     //new vectors based on vector and mag
     const xVecToTarget = this.xTarget-this.x;
     const yVecToTarget = this.yTarget-this.y;
+    const distFromTarget = sqrt(sq(xVecToTarget)+sq(yVecToTarget));
+    const maxAccelerationDist = 20;
+    //if dist is larger than threshold acceleration is maxed
+    if (distFromTarget > maxAccelerationDist){
+      this.acceleration = this.maxAcceleration;
+    } else { //otherwise it is maxed at outter ring of threshold and 0 when dist = 0
+      this.acceleration = (distFromTarget/maxAccelerationDist);
+      this.acceleration = constrain(this.acceleration,this.minAcceleration,this.maxAcceleration);
+    }
+
     const angleToTarget = atan2(yVecToTarget,xVecToTarget);
-    const currentAngle = atan2(this.yVec,this.xVec);
+    this.angle = atan2(this.yVec,this.xVec);
     // console.log(currentAngle);
-    const desiredChangeInAngle = angleToTarget-currentAngle;
-    const newAngle = currentAngle + (desiredChangeInAngle/20);
+    let desiredChangeInAngle;
+    if (abs(angleToTarget-this.angle) < PI) {
+      desiredChangeInAngle = angleToTarget-this.angle;
+    } else { //makes sure if rotating counterclockwise is shortest way to achieve desired angle
+        desiredChangeInAngle = -(angleToTarget-this.angle);
+    }
+    const newAngle = this.angle + (desiredChangeInAngle*this.angularAcceleration);
+
+    //correct the angle when
 
     this.xVec = cos(newAngle) * this.acceleration;
     this.yVec = sin(newAngle) * this.acceleration;
@@ -80,22 +103,31 @@ class Creature {
     //calc vector, normalize vector, recalc vecs
 
   }
+  updateAcceleration(){
+    //slows it down once past radius
+    // xVecToTarget = this.x-
+    // distFromTarget = sqrt(sq(xVecToTarget)+sq(yVecToTarget));
+  }
   updatePositionBasedOnVectors(){
     this.x += this.xVec;
     this.y += this.yVec;
 
+    if (this.radio.value() === true){
+      this.radio.style("display","none");
+    }
+
   }
   display(){
-    const sliderXValue = map(this.x,0,windowWidth,-this.minMaxRange,this.minMaxRange);
-
-    this.xSlider.value(sliderXValue);
-    this.xSlider.style('left', this.x+"px");
-    this.xSlider.style('top', this.y+"px");
+    // const sliderXValue = map(this.x,0,windowWidth,-this.minMaxRange,this.minMaxRange);
+    const btnRadius = 4;
+    this.xSlider.value(this.angle);
+    this.xSlider.style('left', this.x-btnRadius-this.size/2+"px");
+    this.xSlider.style('top', this.y+38+"px");
 
     this.ySlider.style('left', this.x+"px");
     this.ySlider.style('top', this.y+"px");
-    this.radio.style('left', this.x+this.size/2+"px");
-    this.radio.style('top', this.y-this.size/10+"px");
+    this.radio.style('left', this.x-btnRadius+"px");
+    this.radio.style('top', this.y+16+btnRadius+"px");
   }
 
 }
