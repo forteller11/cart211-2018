899b8e9e2544104e15933a5c4af648170e10f617
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-11-26 23:08:43 -0500

    final got sliders vertically oriented via css style.transform

--- final/js/scriptTest.js
+++ final/js/scriptTest.js
@@ -2,8 +2,16 @@ function setup(){
   'use strict';
 let newElement = document.createElement("INPUT");
 newElement.setAttribute("type", "range");
+newElement.setAttribute("orient", "vertical" );
 newElement.textContent = 'ah';
 let body = document.getElementById('bod');
 body.appendChild(newElement);
-console.log(body);
+console.log(newElement);
+
+
+newElement.style.position = "absolute";
+newElement.style.top = "20px";
+newElement.style.width = "200px";
+newElement.style.height = "20px";
+newElement.style.transform = "rotate(270deg)";
 }
