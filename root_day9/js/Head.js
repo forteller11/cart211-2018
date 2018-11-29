b6a7a3afc9ab7fef20a169f059bd395cf93cb335
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-11-29 18:43:40 -0500

    ex9 complete

--- root_day9/js/Head.js
+++ root_day9/js/Head.js
@@ -2,14 +2,22 @@ class Head {
   constructor(){
     this.x = random(windowWidth);
     this.y = random(windowHeight);
+    this.wanderXIndex = random(100000);
+    this.wanderYIndex = random(100000);
+    this.size = 60;
     this.velocity = createVector(random(-this.velocityMax,this.velocityMax), random(-this.velocityMax,this.velocityMax)); //vector to be used as velocity.
 
     let bodyPointer = document.getElementById("bodyID")
-    const videoContainerID = random(1);
-    this.videoContainer = this.createElement("div",videoContainerID,bodyPointer);
+    const hrefID = random(1);
+    this.href = this.createElement("a",hrefID,bodyPointer);
+    this.href.setAttribute("href","https://www.youtube.com/"+round(random(100)));
+    this.href.setAttribute("target","_blank");
+    this.href.style.width = "100px";
+    this.href.style.height = "100px";
+    this.transformElement(this.href, 0, 0, "px");
+    this.href.innerHTML = "youtube.com/"+round(random(100));
+    console.log(this.href);
 
-    const videoID = random(1);
-    this.video = this.createElement("video",videoID,this.videoContainer);
   }
   createElement(typeOfElement, id, pointerOfParent) {
     //parentID where to appendto
@@ -55,10 +63,71 @@ class Head {
     }
   }
 
+  seperate(weight) {
+      let sumDistToheads = createVector(0, 0);
+      let headsWithinRadius = 0;
+      const seperationThreshold = this.size* 2;
+      for (let i = 0; i < heads.length; i++) {
+        const distToheads = createVector(this.x - heads[i].x, this.y - heads[i].y);
+        if (distToheads.mag() < seperationThreshold) { //if within radius, record it, add to summ
+          headsWithinRadius++;
+          const magBasedOnDist = map(distToheads.mag(), 0, seperationThreshold, 250, 0);
+          distToheads.normalize();
+          const addToSum = distToheads.mult(magBasedOnDist);
+          sumDistToheads.add(distToheads);
+        }
+      }
+      let avgDistToheads = sumDistToheads.div(headsWithinRadius);
+      let vectorToTarget = avgDistToheads;
+
+      //VectorToTarget - this.velocity (finds differences in two vectors, or vector which takes velocityvector to vecToTarget)
+      let desiredChangeInVelocity = p5.Vector.sub(vectorToTarget, this.velocity);
+
+      const distToTarget = vectorToTarget.mag();
+
+      let addToVelocity = desiredChangeInVelocity.mult(weight);
+
+        // stroke(0, 0, 255, 40);
+        // noFill();
+        // // ellipse(this.x, this.y, seperationThreshold);
+        // stroke(0, 0, 255);
+        // line(this.x, this.y, this.x + addToVelocity.x, this.y + addToVelocity.y);
+
+
+      this.velocity.add(addToVelocity);
+      //away from neighby x/y positions
+    }
+
+    wander(weight) { //use 2d perlin noise to create random point within unit circle
+        const noiseIncrement = .01;
+        this.wanderXIndex += noiseIncrement;
+        this.wanderYIndex += noiseIncrement;
+        let xTarget = ((noise(this.wanderXIndex)*2)-1)*this.size;
+        let yTarget = ((noise(this.wanderYIndex)*2)-1)*this.size;
+        // ellipse(this.x+xTarget,this.y+yTarget,5);
+        let vectorToTarget = createVector(xTarget,yTarget);
+
+        //VectorToTarget - this.velocity (finds differences in two vectors, or vector which takes velocityvector to vecToTarget)
+        let desiredChangeInVelocity = p5.Vector.sub(vectorToTarget, this.velocity);
+
+        const distToTarget = vectorToTarget.mag();
+
+        let addToVelocity = desiredChangeInVelocity.mult(weight*.01);
+
+
+          stroke(0, 0, 255);
+          line(this.x, this.y, this.x + addToVelocity.x, this.y + addToVelocity.y);
+
+
+        this.velocity.add(addToVelocity);
+        //away from neighby x/y positions
+      }
+
+
   updatePositionOfElements() {
     let xOffset = this.size / 1.7;
     let yOffset = this.size / 4;
-    this.transformElement(this.videoContainer, this.x, this.y, "px");
+    this.transformElement(this.href, this.x-this.size/2, this.y, "px");
   }
 
   addVelocityToPosition() {
@@ -68,10 +137,12 @@ class Head {
   }
 
   update() {
+    // ellipse(this.x,this.y,20);
     // this.seekMouse(0.0001);
     // this.align(.1);
     // this.clump(.001);
-    // this.seperate(.01);
+    this.seperate(.01);
+    this.wander(.1);
 
     // this.maintainDistance(.01,this.distToMaintain);
     this.addVelocityToPosition();
--- root_day9/js/main.js
+++ root_day9/js/main.js
@@ -1,11 +1,13 @@
 let heads = [];
 
 function setup(){
-  for (let i = 0; i < 3; i ++){
+  createCanvas(windowWidth,windowHeight);
+  for (let i = 0; i < 200; i ++){
   heads[i] = new Head;
   }
 }
 function draw(){
+background(255);
   for (let i = 0; i < heads.length; i ++){
   heads[i].update();
   }
